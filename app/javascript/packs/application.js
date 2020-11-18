b0b960c1332bbeb14b85ff1bf27e63129fe5b58c
Josexd2810 <josefigueroa2810@gmail.com>
2020-11-18 15:44:13 -0400

    Template de bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,10 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+require("jquery")
+require("pepper")
+require("bootstrap")
+require("sb-admin-2.min")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
